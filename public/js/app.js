07347b1c14381fbaa70ddf932f4708b9399f266a
Azur_Azabache <tharuka_20@hotmail.com>
2018-02-18 18:03:02 -0500

    subiendo archivos

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,15 @@ $(document).ready(function() {
   var $textArea = $('#icon_prefix2');
   var $btnPostText = $('#btnPost');
   var $btnPostImage = $('#btnPostImage');
+  var $btnPostEvent = $('#btnPostEvent');
   var $container = $('#container-posts');
   moment.locale('es');
   var $date = moment().format('lll');
   var $postImage= $('#postImage');
   var $inputText= $('#contentInput');
   var $inputImage= $('#contentInput2');
+  var $inputEvent= $('#inputEvent');
+
   var img = 'bleble';
   $textArea.focus();
   // Función para habilitar boton de post
@@ -24,6 +27,7 @@ $(document).ready(function() {
     var $text = $textArea.val();
       $($container).prepend('<div class="postear"><span class="date">' + $date + '</span><br><br><p>' + $text + '</p></div>');
       $textArea.val('');
+      $btnPostText.addClass('disabled');
   });
 
 //Función para postear imagen
@@ -34,6 +38,8 @@ $(document).ready(function() {
   alert('No se puede cargar archivos en esta navegador, porfavor use otro');
 }
 $('#path').val('');
+$inputImage.val('');
+$btnPostImage.addClass('disabled');
   });
 
 //Para cargar imagenes
@@ -48,19 +54,79 @@ $('#path').val('');
      console.log($('#file').val());
    });
 
+//Para postear evento con ubicacion
+  $('#first_name').on('input', function(){
+    if($inputEvent.val() === '' && $('#first_name').val() === ''){
+      $btnPostEvent.addClass('disabled');
+    }else{
+      $btnPostEvent.removeClass('disabled');
+    }
+  });
+
+  $btnPostEvent.on('click', function(){
+      text2= $('#first_name').val();
+      eve = $inputEvent.val();
+      console.log(eve);
+        $($container).prepend('<div class="postear"><span class="date">' + $date + '</span><br><br><span>'+ text2 +'</span><p>Día del Evento: '+eve+'</p></div>');
+      $btnPostEvent.addClass('disabled');
+      $inputEvent.val('');
+      $('#first_name').val('');
+  });
+
+//Datepicker
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
+
 //Función para aparecer input de imagen
 $postImage.on('click', function(){
   $inputText.addClass('hide');
   $btnPostText.addClass('hide');
+  $btnPostEvent.addClass('hide');
+  $('#contentEvent').removeClass('hide');
   $inputImage.removeClass('hide');
   $btnPostImage.removeClass('hide');
+  $('#btnPostVideo').addClass('hide');
+  $('#contentVideo').addClass('hide');
   });
 
 //Función para aparecer input de texto
 $('#postText').on('click', function(){
     $inputText.removeClass('hide');
     $btnPostText.removeClass('hide');
+    $btnPostEvent.addClass('hide');
+    $('#contentEvent').removeClass('hide');
     $inputImage.addClass('hide');
     $btnPostImage.addClass('hide');
+    $('#btnPostVideo').addClass('hide');
+    $('#contentVideo').addClass('hide');
     });
+
+//Función para aparecer input de evento
+  $('#postEvent').on('click', function(){
+    $inputText.addClass('hide');
+    $btnPostText.addClass('hide');
+    $btnPostEvent.removeClass('hide');
+    $('#contentEvent').removeClass('hide');
+    $inputImage.addClass('hide');
+    $btnPostImage.addClass('hide');
+    $('#btnPostVideo').addClass('hide');
+    $('#contentVideo').addClass('hide');
+    });
+//Funcion para aparecer input de video
+  $('#postVideo').on('click', function(){
+    $inputText.addClass('hide');
+    $btnPostText.addClass('hide');
+    $btnPostEvent.addClass('hide');
+    $('#contentEvent').addClass('hide');
+    $('#btnPostVideo').removeClass('hide');
+    $('#contentVideo').removeClass('hide');
+    $inputImage.addClass('hide');
+    $btnPostImage.addClass('hide');
+  });
 });
